dbc0f83d42e21f332f7c71e534817f12f2b3ccad
Pedro Figueiredo <pedrohfm2012@yahoo.com.br>
2024-06-25 23:16:54 -0300

    Organizando o service TaxaCambio

--- app/Services/TaxaCambio.php
+++ app/Services/TaxaCambio.php
@@ -8,34 +8,9 @@ class TaxaCambio
 {
     public function getTaxaCambio($moeda)
     {
-        // $dataCotacao = date('m-d-Y', strtotime('-1 days'));
-
-        // $resposta = Http::get("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='" . $moeda . "'&@dataCotacao='06-21-2024'&\$top=100&\$skip=4&\$format=json&\$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao");
-
-        // $resultado = null;
-
-        // if ($resposta->successful()) {
-
-        //     $resultado = $resposta->json();
-
-        //     if (!empty($resposta['value'])) {  //! retorna true se tiver vazio | retorna false se tiver valor
-
-        //         // $cotacaoExtraida = json_decode($resultado, true);
-        //         $cotacaoCompra = $resultado['value'][0]['cotacaoCompra'];
-        //         $cotacaoVenda = $resultado['value'][0]['cotacaoVenda'];
-
-        //         return response()->json([
-        //             'cotacaoCompra' => $cotacaoCompra,
-        //             'cotacaoVenda' => $cotacaoVenda,
-        //         ]);
-        //     } else {
-        //         return response()->json(['Erro:' => 'Nenhum dado encontrado.'], 500);
-        //     }
-        // }
-
         $dataCotacao = date('m-d-Y', strtotime('-1 days'));
 
-        $resposta = Http::get("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='" . $moeda . "'&@dataCotacao='06-21-2024'&\$top=100&\$skip=4&\$format=json&\$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao");
+        $resposta = Http::get("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='" . $moeda . "'&@dataCotacao='" . $dataCotacao . "'&\$top=100&\$skip=4&\$format=json&\$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao");
 
         if ($resposta->successful()) {
             $resultado = $resposta->json();
